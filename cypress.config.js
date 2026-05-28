23fec9ce000f996ea98304d24d82a009da978857
gracchusBBF <raphaelmiceli1988@gmail.com>
2026-05-28 11:58:52 +0200

    init cypress

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {},
   },
 })
